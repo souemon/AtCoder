5db799ab4934929257f02a5e667959394f2f9f4a
souemon <kobekobekitakita@gmail.com>
2024-06-23 16:28:47 +0900

    20240623

---
+++ src/AtCoder Beginners Selection/04_ABC081B - Shift only.ts
@@ -0,0 +1,20 @@
+import * as fs from "fs";
+
+// argsに入力データ全体が入る
+function Main(args: any): void {
+  const numbers: number[] = args
+    .split("\n")[1]
+    .split(" ")
+    .map((num: string) => +num);
+  let i: number = 0;
+  type conditionFunction = (i: number) => boolean;
+  const condition: conditionFunction = (i: number): boolean => {
+    return numbers.every((num: number) => num % 2 ** i === 0);
+  };
+  do {
+    i++;
+  } while (condition(i));
+  console.log(i - 1);
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(fs.readFileSync(process.stdin.fd, "utf8"));
