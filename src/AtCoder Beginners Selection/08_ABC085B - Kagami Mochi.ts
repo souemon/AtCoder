ae624f30176aa903098dcb62db884bacb52c8928
souemon <kobekobekitakita@gmail.com>
2024-06-25 21:44:09 +0900

    20240625

--- src/AtCoder Beginners Selection/08_ABC085B - Kagami Mochi.ts
+++ src/AtCoder Beginners Selection/08_ABC085B - Kagami Mochi.ts
@@ -1,12 +1,13 @@
 import * as fs from "fs";
 
 // argsに入力データ全体が入る
-function Main(args: any): void {
-  const [N, ...dList]: string[] = args.split("\n");
-  const SortedD: number[] = Array.from(
-    new Set(dList.map((number) => parseInt(number.trim(), 10)))
-  );
-  console.log(SortedD.length);
+function Main(args: string): void {
+  const inputData = args.split("\n");
+  const dList: number[] = inputData
+    .slice(1, inputData.length - 1)
+    .map((input) => parseInt(input));
+  const result: number[] = Array.from(new Set(dList));
+  console.log(result.length);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 Main(fs.readFileSync(process.stdin.fd, "utf8"));
