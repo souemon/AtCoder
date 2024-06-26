4febefe26961d3871616f44221904a1deb471196
souemon <kobekobekitakita@gmail.com>
2024-06-26 21:45:19 +0900

    20240626

---
+++ src/AtCoder Beginners Selection/09_ABC085C - Otoshidama.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+// argsに入力データ全体が入る
+function Main(args: string): void {
+  const inputData: number[] = args.split(" ").map(Number);
+  const N: number = inputData[0];
+  const Y: number = inputData[1];
+  let result: boolean = false;
+  for (let a = N; a >= 0; a--) {
+    for (let b = N - a; b >= 0; b--) {
+      if (10000 * a + 5000 * b + 1000 * (N - a - b) == Y) {
+        console.log(`${a} ${b} ${N - a - b}`);
+        let result: boolean = true;
+        return;
+      }
+    }
+  }
+  if (!result) {
+    console.log("-1 -1 -1");
+  }
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(fs.readFileSync(process.stdin.fd, "utf8"));
