4f607f119def074c96476fe04d8e8d4fe3bca9f7
souemon <kobekobekitakita@gmail.com>
2024-07-02 22:39:41 +0900

    20240702

--- src/AtCoder Beginners Selection/11_ABC086C - Traveling.ts
+++ src/AtCoder Beginners Selection/11_ABC086C - Traveling.ts
@@ -1,53 +1,39 @@
 import * as fs from "fs";
-import { stringify } from "querystring";
 
 // argsに入力データ全体が入る
 function Main(args: string): void {
-  const [N, ...dataList] = args.split("\n");
+  const [N, ...dataList] = args.trim().split("\n");
   const numberN: number = parseInt(N, 10);
   const dataListNum: number[][] = dataList.map((data) =>
     data.split(" ").map(Number)
   );
-  let t: number = 0;
-  let x: number = 0;
-  let y: number = 0;
-  let xAndY: number = 0;
-  let result: string = "No";
-  for (let i: number = 0; i < numberN; i++) {
-    const diffT: number = Math.abs(t - dataListNum[i][0]);
-    const diff: number = dataListNum[i][1] + dataListNum[i][2];
-    t = dataListNum[i][0];
-    if (diffT % 2 == 0) {
-      if (
-        diffT >=
-          Math.abs(Math.abs(dataListNum[i][1]) - Math.abs(x)) +
-            Math.abs(Math.abs(dataListNum[i][2]) - Math.abs(y)) &&
-        (diff / diffT == 1 || diff == 0)
-      ) {
-        result = "Yes";
-      } else {
-        result = "No";
-        break;
-      }
-    } else {
-      let flag: boolean = false;
-      for (let j: number = diffT; j > 0; j = j - 2) {
-        if (Math.abs(xAndY - diff) == Math.abs(j)) {
-          flag = true;
-          result = "Yes";
-          break;
-        }
-      }
-      xAndY = dataListNum[i][1] + dataListNum[i][2];
-      if (!flag) {
-        result = "No";
-        break;
-      }
+
+  let t = 0,
+    x = 0,
+    y = 0;
+  let result = "Yes";
+
+  for (let i = 0; i < numberN; i++) {
+    const [nextT, nextX, nextY] = dataListNum[i];
+    const diffT = nextT - t;
+    const diffX = Math.abs(nextX - x);
+    const diffY = Math.abs(nextY - y);
+    const distance = diffX + diffY;
+
+    // 移動可能かどうかの判定
+    if (diffT < distance || (diffT - distance) % 2 !== 0) {
+      result = "No";
+      break;
     }
-    x = dataListNum[i][1];
-    y = dataListNum[i][2];
+
+    // 次の地点に更新
+    t = nextT;
+    x = nextX;
+    y = nextY;
   }
+
   console.log(result);
 }
+
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 Main(fs.readFileSync(process.stdin.fd, "utf8"));
